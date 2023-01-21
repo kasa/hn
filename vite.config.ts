bfe97a195a0a2246498620d8e1718811630eb717
Eduardo Kasa <ekasa@redventures.com>
2023-01-21 08:57:57 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import solidPlugin from "vite-plugin-solid";
+import WindiCSS from "vite-plugin-windicss";
+
+export default defineConfig({
+	plugins: [
+		solidPlugin(),
+		WindiCSS(),
+		{
+			// default settings on build (i.e. fail on error)
+			...eslint(),
+			apply: "build",
+		},
+		{ // do not fail on serve (i.e. local development)
+			...eslint({
+				failOnWarning: false,
+				failOnError: false,
+			}),
+			apply: "serve",
+			enforce: "post"
+		}
+	],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+	},
+});
